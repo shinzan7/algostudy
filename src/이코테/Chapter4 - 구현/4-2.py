31ff6b1e12600a8e08e361c9d8a11de35a49e413
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-10-10 21:54:04 +0900

    221010

--- src/이코테/Chapter4 - 구현/4-2.py
+++ src/이코테/Chapter4 - 구현/4-2.py
@@ -1,30 +1,20 @@
-N = int(input())
-# N = 5
+order = input()
+# order = "a1"
 
-class Clock():
-    def __init__(self):
-        self.h = 0
-        self.m = 0
-        self.s = 0
-    def tick(self):
-        if(self.s != 59):
-            self.s += 1
-        else:
-            self.s = 0
-            if(self.m != 59):
-                self.m += 1
-            else:
-                self.m = 0
-                self.h += 1
+c = ord(order[0]) - ord("a") + 1
+r = int(order[1])
 
-clock = Clock()
+def isIn(r, c):
+    return r>=1 and r<=8 and c>=1 and c<=8
+
+# 상하좌우 2개씩
+dx = [1,-1,1,-1,-2,-2,2,2]
+dy = [-2,-2,2,2,1,-1,1,-1]
+
 count = 0
 
-while(clock.h != N or clock.m != 59 or clock.s != 59):
-    now = str(clock.h) + str(clock.m) + str(clock.s)
-    if("3" in now):
+for idx in range(8):
+    if(isIn(r+dy[idx], c+dx[idx])):
         count += 1
-    clock.tick()
 
 print(count)
-
--- src/이코테/Chapter4 - 구현/4-3.py
+++ src/이코테/Chapter4 - 구현/4-3.py
@@ -1,20 +1,65 @@
-order = input()
-# order = "a1"
+# 4-3 게임 개발
 
-c = ord(order[0]) - ord("a") + 1
-r = int(order[1])
+n,m = map(int, input().split())
+# m, n = 4, 4
 
-def isIn(r, c):
-    return r>=1 and r<=8 and c>=1 and c<=8
+#방문배열
+is_visit = [[False] * m for i in range(n)]
 
-# 상하좌우 2개씩
-dx = [1,-1,1,-1,-2,-2,2,2]
-dy = [-2,-2,2,2,1,-1,1,-1]
+#북동남서 0123
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
 
-count = 0
+r, c, dir = map(int, input().split())
+# r, c, dir = 1, 1, 0
 
-for idx in range(8):
-    if(isIn(r+dy[idx], c+dx[idx])):
-        count += 1
+game_map = []
+for i in range(n):
+    game_map.append(list(map(int, input().split())))
 
-print(count)
+class Character():
+    def __init__(self,r ,c ,dir):
+        self.r = r
+        self.c = c
+        self.dir = dir
+        self.count = 0
+    def move(self, left):
+        self.r += dr[left]
+        self.c += dc[left]
+
+cur = Character(r, c, dir)
+is_visit[r][c] = True
+cur.count += 1
+
+turn_count = 0
+while True:
+    left = (cur.dir + 3) % 4
+    nr = cur.r + dr[left]
+    nc = cur.c + dc[left]
+    # 사방이 바다거나 갔던 곳이면 뒤로 이동
+    if(turn_count == 4):
+        back = (cur.dir + 2) % 4
+        br = cur.r + dr[back]
+        bc = cur.c + dc[back]
+        #뒤로 이동할 수 있는 경우
+        if(game_map[br][bc] == 0):
+            cur.r = br
+            cur.c = bc
+            continue
+        #뒤로 이동할 수 없는 경우
+        else:
+            break
+
+    # 왼쪽방향 갈 수 있으면 이동
+    if(is_visit[nr][nc] == False and game_map[nr][nc] == 0):
+        cur.dir = left
+        cur.move(left)
+        turn_count = 0
+        is_visit[nr][nc] = True
+        cur.count += 1
+    # 갈 수 없으면 회전만하기
+    else:
+        cur.dir = left
+        turn_count += 1
+
+print(cur.count)
